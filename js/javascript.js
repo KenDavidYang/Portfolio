f7ef400ab2054e9609a041ecd981fb65afb12629
default <seconenkacc@gmail.com>
2025-12-16 13:41:45 +0800

    Add: Item Modal
    Chore: standardize name
    
    Fix: .draggable cursor

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,12 @@
 const draggable = document.querySelectorAll('.draggable');
+const dblClick = document.querySelectorAll('.dbl-click');
 
 let viewportWidth = window.innerWidth;
+let itemWidth       = document.querySelector(".item-modal").offsetWidth;
+console.log(itemWidth);
+
+
+
 function pxToVw(px) {
     return (px / viewportWidth) * 100;
 }
@@ -27,27 +33,34 @@ draggable.forEach(draggable => {
 
     dragInitiator.addEventListener("mousedown", (event) => {
         event.preventDefault(); 
-
+        // tbf dragging outside
         isDragging = true;
+        if(event.clientX >= itemWidth) {
+            console.log("outside");
+            console.log(itemWidth);
+        }
         offsetX = event.clientX - draggable.offsetLeft;
         offsetY = event.clientY - draggable.offsetTop;
 
         if (dragHandle == dragInitiator) {
             draggable.style.cursor = "move";
         }
+        draggable.style.cursor = "move";
         console.log(isDragging);
     });
 
     document.addEventListener("mouseup", () => {
         isDragging = false;
 
-        draggable.style.cursor = "auto";
+        draggable.style.cursor = "pointer";
         console.log(isDragging);
     });
 
+
+    // remove?
     draggable.addEventListener("dblclick", () => {
         const link = draggable.getAttribute("link");
-        if(link) {
+        if(link && link.startsWith("#")) {
             const modal = document.querySelector(link);
             modal.style.display = "block";
         }
@@ -58,8 +71,17 @@ draggable.forEach(draggable => {
     });
 })
 
-document.addEventListener("contextmenu", function(e){
+document.addEventListener("contextmenu", (e) => {
     if (e.target.nodeName === "IMG") {
         e.preventDefault();
     }
 }, false);
+
+dblClick.forEach(anchor => {
+    anchor.addEventListener("dblclick", (event) => {
+        event.preventDefault();
+
+        const link = anchor.getAttribute("link");
+        window.open(link, "_blank");
+    });
+});
